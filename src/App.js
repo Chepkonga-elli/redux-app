5cef8dc626e9b50a194cc262f219a46025971e79
Chepkonga-elli <kiptoochelimo219@gmail.com>
2023-02-04 23:05:50 +0300

    Added several files Posts, PostForm mainly

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+import { Provider } from 'react-redux';
+import { configureStore } from "@reduxjs/toolkit"
+// import { applyMiddleware } from 'redux';
+
+// export const store = configureStore(() => [], {}, applyMiddleware())
+export const store = configureStore({
+  reducer: {
+      Posts: [],
+  },
+  middleware: getDefaultMiddleware =>
+      getDefaultMiddleware(),
+  devTools: true
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <PostForm/>
+        <hr />
+        <Posts/>
+      </div>
+    </Provider>
   );
 }
 
